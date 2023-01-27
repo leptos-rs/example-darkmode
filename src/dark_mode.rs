bdcd715bedac295b157345585cff051e769bec2b
Greg Johnston <greg.johnston@gmail.com>
2023-01-27 12:45:03 -0500

    Correct the implementation of `prefers_dark` to fix bug at 49:24 and correct behavior of `<ActionForm/>`

--- src/dark_mode.rs
+++ src/dark_mode.rs
@@ -33,18 +33,17 @@ fn initial_prefers_dark(_cx: Scope) -> bool {
 
 #[cfg(feature = "ssr")]
 fn initial_prefers_dark(cx: Scope) -> bool {
-    if let Some(req) = use_context::<actix_web::HttpRequest>(cx) {
-        let cookies = req.cookies();
-        cookies
-            .map(|cookies| {
-                cookies
-                    .iter()
-                    .any(|cookie| cookie.name() == "darkmode" && cookie.value() == "true")
-            })
-            .unwrap_or(false)
-    } else {
-        false
-    }
+    use_context::<actix_web::HttpRequest>(cx)
+        .and_then(|req| {
+            req.cookies()
+                .map(|cookies| {
+                    cookies
+                        .iter()
+                        .any(|cookie| cookie.name() == "darkmode" && cookie.value() == "true")
+                })
+                .ok()
+        })
+        .unwrap_or(false)
 }
 
 #[component]
@@ -52,13 +51,24 @@ pub fn DarkModeToggle(cx: Scope) -> impl IntoView {
     let initial = initial_prefers_dark(cx);
 
     let toggle_dark_mode_action = create_server_action::<ToggleDarkMode>(cx);
+    // input is `Some(value)` when pending, and `None` if not pending
+    let input = toggle_dark_mode_action.input();
+    // value contains most recently-returned value
+    let value = toggle_dark_mode_action.value();
 
+    // NOTE: if you're following along the with video, this was implemented
+    // incorrectly at the time I made it, due to a bug in <ActionForm/> that
+    // was not resetting input. This is how it should have been implemented
+    // all along, which would also have fixed the bug at 49:24!
     let prefers_dark = move || {
-        toggle_dark_mode_action
-            .input()
-            .get()
-            .map(|submission| submission.prefers_dark)
-            .unwrap_or(initial)
+        match (input(), value()) {
+            // if there's some current input, use that optimistically
+            (Some(submission), _) => submission.prefers_dark,
+            // otherwise, if there was a previous value confirmed by server, use that
+            (_, Some(Ok(value))) => value,
+            // otherwise, use the initial value
+            _ => initial,
+        }
     };
 
     let color_scheme = move || {
